a0ad758f092449ff3c9b515b73db2e17718fc92c
Pablo Novaes <133129982+PabloNovaes@users.noreply.github.com>
2024-03-15 09:47:39 -0300

    Atualizar o projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -27,7 +27,7 @@ export const projects: Record<string, ProjectsProps> = {
     },
     'ts-table': {
         name: 'Ts-Table',
-        description: 'Este foi o primeiro projeto em React que desenvolvi sozinho sem auxilio de curso ou coisa do tipo,e por mais simples que seja, tive um aprendizado significativo em react e ts. É uma tabela com dadoa estáticos, podendo ser feita a inclusao de um novo produto e aplicação de filtros.',
+        description: 'Este foi o primeiro projeto em React que desenvolvi sozinho sem auxilio de curso ou coisa do tipo, e por mais simples que seja, tive um aprendizado significativo em react e ts. É uma tabela com dados estáticos, podendo ser feita a inclusao de um novo produto e aplicação de filtros.',
         image: '../assets/projects/ts-table.jpg',
         stack: ['TypeScript', 'React', 'Tailwind', 'Shadcn-ui'],
         codeUrl: 'https://github.com/pablonovaes/ts-table',
@@ -36,7 +36,7 @@ export const projects: Record<string, ProjectsProps> = {
     },
     'ts-form': {
         name: 'Ts-Form',
-        description: 'Um formulario em react com ts, onde pude praticar ainda mais o uso do Zod para lidar com o formulario (desde sua configuração no projeto, até o seu uso em si), não possui muitas validações devido a simplicidade do projeto.',
+        description: 'Um formulario em react com ts, onde pude praticar ainda mais o uso do Zod para lidar com formulario (desde sua configuração no projeto, até o seu uso em si), seu uso não foi muito "intenso" devido a simplicidade do projeto.',
         image: '../assets/projects/ts-form.jpg',
         stack: ['TypeScript', 'React', 'Tailwind', 'Shadcn-ui', 'Zod'],
         codeUrl: 'https://github.com/pablonovaes/ts-form',
