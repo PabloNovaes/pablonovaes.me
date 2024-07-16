1d7bec002564cea3bc87d2aca7e90b6042b3a848
PabloNovaes <ppablo.0028922@gmail.com>
2024-07-16 20:54:25 -0300

    update metatags

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react"
 import path from "path"
 import { defineConfig } from "vite"
- 
+
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
